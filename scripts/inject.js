fefbb8b97a59dc6bd898b8e97891f5f6f645d321
govipg <govipg@126.com>
2025-01-17 00:04:54 +0800

    add0.3.7未完成的上传

---
+++ scripts/inject.js
@@ -0,0 +1,3 @@
+// 霞骛文楷字体
+hexo.extend.injector.register('head_end', '<link rel="stylesheet" href="https://unpkg.com/lxgw-wenkai-screen-webfont@1.7.0/lxgwwenkaiscreen.css">');
+hexo.extend.injector.register('head_end', '<style>body { font-family: "LXGW WenKai Screen", sans-serif; font-size:18px; }</style>');
